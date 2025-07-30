6061984846377a8bcae43c9ac4eb2c7b4bc12655
abrakingoo <maikoabraham68@gmail.com>
2025-07-30 12:54:02 +0300

    feat: creat a basic calculator functionality

---
+++ modules/python/week1_Intro_to_Python_Assignment/calculator.py
@@ -0,0 +1,27 @@
+# Basic Calculator Program
+
+#Ask User For Input
+
+num1 = int(input("Enter The First Number: "))
+num2 = int(input("Enter The Second Number: "))
+
+operation = input("Enter an operation (+, -, *, /): ")
+
+# Perform the calculation based on the operation
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. Please enter one of +, -, *, /.")

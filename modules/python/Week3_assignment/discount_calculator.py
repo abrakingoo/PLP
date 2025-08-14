7cef8b7e935e9210f3fa291ef947590663d2e27f
abrakingoo <maikoabraham68@gmail.com>
2025-08-14 08:01:00 +0300

    chore: add comment and clear output depending on whether the discount was applied

--- modules/python/Week3_assignment/discount_calculator.py
+++ modules/python/Week3_assignment/discount_calculator.py
@@ -1,11 +1,20 @@
 def calculate_discount(price, discount_percent):
-    if price < 0 or discount_percent < 0:
-        return "Invalid input. Price and discount must be non-negative."
+    #calculates and return the new discounted price
     if discount_percent >= 20:
         discount_amount = (price * discount_percent) / 100
         return price - discount_amount
-    return price
+    else:
+        return price
 
+# get user input
 price = int(input("Enter The Original Price Of The Item: "))
 discount_percent = int(input("Enter The Discount Percentage: "))
-print(f'The Final Discounted Price Is {calculate_discount(price, discount_percent)}')
+
+# calculate the discout
+discounted_price = calculate_discount(price, discount_percent)
+
+# print results
+if discounted_price < price:
+    print(f'The Final Discounted Price Is {discounted_price}')
+else:
+    print(f'The Discount Was Less Than 20%, Price Remains: {discounted_price}')
